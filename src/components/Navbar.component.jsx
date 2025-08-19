8aa765227f71341ba936b6b4f045038833b1ca7c
Kye-Lee <kyelee.low@gmail.com>
2025-08-19 10:28:55 +0100

    refactor: reorganise components into separate files

---
+++ src/components/Navbar.component.jsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+
+function Navbar({ movies }) {
+  const [query, setQuery] = useState("");
+
+  return (
+    <nav className="nav-bar">
+      <div className="logo">
+        <span role="img">🍿</span>
+        <h1>Popcorn Ratings</h1>
+      </div>
+      <input
+        className="search"
+        type="text"
+        placeholder="Search movies..."
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+      />
+      <p className="num-results">
+        Found <strong>{movies.length}</strong> results
+      </p>
+    </nav>
+  );
+}
+
+export default Navbar;
---
+++ src/components/WatchedBox/WatchedMoviesList.component.jsx
@@ -0,0 +1,13 @@
+import WatchedMovie from "./WatchedMovie.component";
+
+function WatchedMoviesList({ watched }) {
+  return (
+    <ul className="list">
+      {watched.map((movie) => (
+        <WatchedMovie movie={movie} key={movie.imdbID} />
+      ))}
+    </ul>
+  );
+}
+
+export default WatchedMoviesList;
